531e9097bdb0e78e3fee73adaf643e653ac9cd7c
kyunhyun94 <ekfqlwcjswl@naver.com>
2022-02-24 15:58:41 +0900

    ERC721

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # NFT_Dapp
+## 기술 
+- 솔리디티
+- Open Zeppelin 
+- React
+- chakra
+- web3.js
+
+---
+## 설치
+GitHub https://github.com/h662/h662Animals-contracts/commit/e906e831d2ab199b9cd6b5832f8e61412b2107cd 
+
+Visual Studio Code https://code.visualstudio.com/Download 
+
+NodeJS https://nodejs.org/ko/download/ 
+
+Remix IDE https://remix.ethereum.org/
+
+---
 
 ```
  npm install @openzeppelin/contracts
+```
+test 
+```
+remixed - s. —remix-ide https://remix.ethereum.org
 ```
---
+++ contracts/MintAnimalToken.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+
+
+contract MintAnimalToken is ERC721Enumerable {
+    constructor() ERC721("h662Animals", "HAS") {} //한번 실행 //name symbol
+
+    mapping(uint256 => uint256) public animalTypes;
+
+    function mintAnimalToken() public {
+        uint256 animalTokenId = totalSupply() + 1;
+
+        uint256 animalType = uint256(keccak256(abi.encodePacked(block.timestamp, msg.sender, animalTokenId))) % 5 + 1;
+
+        animalTypes[animalTokenId] = animalType;
+//animalTokenId = nft가 가지는 유일한 값
+//totalSupply = 민팅된 nft양
+//msg.sender -> minting누른사람
+        _mint(msg.sender, animalTokenId);//ERC721 ->에서 지원
+    }
+} 
